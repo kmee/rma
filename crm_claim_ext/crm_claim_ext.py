ed1df1b7041b30fafda4e072500180a9e8dd88d9
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2015-01-30 18:06:15 -0430

    [ADD] add again the canal_id field but using a new model 'crm.tracking.medium' taking into account http://goo.gl/fgF2AD

--- crm_claim_ext/crm_claim_ext.py
+++ crm_claim_ext/crm_claim_ext.py
@@ -34,6 +34,7 @@ class crm_claim_ext(osv.osv):
     _description = "Add some fields to crm_claim"
     _inherit = 'crm.claim'
     _columns = {
+        'canal_id': fields.many2one('crm.tracking.medium', 'Channel'),
         'som': fields.many2one('res.partner.som', 'State of Mind'),
                 # Aftersale outsourcing
 #        'in_supplier_picking_id': fields.many2one('stock.picking', 'Return To Supplier Picking', required=False, select=True),
